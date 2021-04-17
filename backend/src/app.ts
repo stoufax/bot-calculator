ce18f36132230a4fd93c37f39680e0f10a03a8d0
Mustafa Mokrani <mokrani.mustafa@gmail.com>
2021-04-17 16:06:50 +0100

    [backend][frontend] Setting up real time node server and react app

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import * as http from "http";
+import { Server } from "socket.io";
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.set("port", port);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
